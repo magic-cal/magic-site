b6da904ad340950093abca4479f6bce64a0b8693
CJ Mcclure <callum.mcclure98@gmail.com>
2022-05-17 22:16:30 +0100

    WIP about page

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" align="center" no-gutters>
+    <v-col cols="12" pa-0>
+      <heading-section title="About" subtitle="A little bit about Callum" />
+      <!-- <v-sheet class="pb-6 pt-12 text-center" color="white">
+          <v-img :src="require('@/static/shuffle-cropped.jpg')"> </v-img>
+        </v-sheet> -->
+    </v-col>
+    <v-col> </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import { allFaqs } from '@/content/faqs'
+
+export default defineComponent({
+  setup() {
+    return { allFaqs }
+  },
+})
+</script>
